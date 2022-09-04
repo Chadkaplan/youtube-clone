9186f6850f5fe061ba279020f77920de8acc4d2d
Chad Kaplan <chadkaplan@knights.ucf.edu>
2022-09-03 23:12:55 -0400

    Added links to resources to be used in index and added basic style resets.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
-}
+import React from "react";
+
+const App = () => {
+  return <div>App</div>;
+};
 
 export default App;
