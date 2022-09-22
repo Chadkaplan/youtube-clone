193af4e72776200d0e60414df06ec849df07240d
Chad Kaplan <chadkaplan@knights.ucf.edu>
2022-09-21 23:23:54 -0400

    Finished dynamic fetchFromAPI util. Need to test integration of fetchFromAPI.

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,4 +1,4 @@
-const axios = require("axios");
+import axios from "axios";
 
 const BASE_URL = "https://youtube-v31.p.rapidapi.com/search";
 
@@ -13,11 +13,8 @@ const options = {
   },
 };
 
-axios
-  .request(options)
-  .then(function (response) {
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    console.error(error);
-  });
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
